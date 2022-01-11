6a2385dabe1395723ebf61e320d845c393468e50
nuatli <33758305+nuatli@users.noreply.github.com>
2022-01-12 01:57:14 +0300

    Update index.js
    index.js duzenlendi

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import {Provider} from 'react-redux';
-import './bootstrap-override.scss';
 import './css/index.css';
 import registerServiceWorker from './registerServiceWorker';
 import {StateProvider} from './store/StateProvider';
@@ -9,15 +7,7 @@ import reducer,{initialState} from './reducer';
 
 import App from './App';
 
-/*
-ReactDOM.render(
-		<Provider >
-			<LoginPage />
-			<LanguageSelector />
-		</Provider>
-		,document.getElementById("root")
-);
-*/
+
 ReactDOM.render(
 		<React.StrictMode>
 			<StateProvider initialState={initialState} reducer={reducer}>
